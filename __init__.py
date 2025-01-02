e2bad2a572cfec310dfc09b01ba257dddb9c42a9
Priyansh Bhatter <pbhatter@usc.edu>
2025-01-03 02:51:32 +0530

    init updated

--- __init__.py
+++ __init__.py
@@ -1,27 +1,3 @@
-# from .playbookFalNodes import (
-#     Playbook_FalClient,
-#     Playbook_MiniMaxHailuo,
-#     Playbook_Kling,
-# )
-# from .playbookVideoNodes import PlaybookVideoPreview
-
-# NODE_CLASS_MAPPINGS = {
-#     "Playbook FalClient": Playbook_FalClient,
-#     "Playbook MiniMaxHailuo": Playbook_MiniMaxHailuo,
-#     "Playbook Kling": Playbook_Kling,
-#     "Playbook Video Preview": PlaybookVideoPreview,
-
-# }
-
-# NODE_DISPLAY_NAME_MAPPINGS = {
-#     "Playbook FalClient": "Playbook Fal Client",
-#     "Playbook MiniMaxHailuo": "Playbook MiniMax/Hailuo Video",
-#     "Playbook Kling": "Playbook Kling Video",
-#     "Playbook Video Preview": "Playbook Video Preview",
-# }
-
-# __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
-
 from .playbookFalNodes import (
     Playbook_MiniMaxHailuo,
     Playbook_Kling,
