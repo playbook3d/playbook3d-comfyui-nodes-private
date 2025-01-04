0d38b34bd9bf72c38b7ace984f1f2ca3cc9c9453
Priyansh Bhatter <pbhatter@usc.edu>
2025-01-05 03:26:39 +0530

    updated init

--- __init__.py
+++ __init__.py
@@ -3,8 +3,6 @@ from .playbookFalNodes import (
     Playbook_Kling,
 )
 
-from .playbookVideoNodes import PlaybookVideoPreview
-
 from .lumaDreamMachineNode import (
     Playbook_LumaAIClient,
     Playbook_Text2Video,
@@ -17,7 +15,6 @@ from .lumaDreamMachineNode import (
 NODE_CLASS_MAPPINGS = {
     "Playbook MiniMaxHailuo": Playbook_MiniMaxHailuo,
     "Playbook Kling": Playbook_Kling,
-    "Playbook Video Preview": PlaybookVideoPreview,
     "Playbook LumaAIClient": Playbook_LumaAIClient,
     "Playbook Text2Video": Playbook_Text2Video,
     "Playbook Image2Video": Playbook_Image2Video,
@@ -29,7 +26,6 @@ NODE_CLASS_MAPPINGS = {
 NODE_DISPLAY_NAME_MAPPINGS = {
     "Playbook MiniMaxHailuo": "Playbook MiniMax/Hailuo Video",
     "Playbook Kling": "Playbook Kling Video",
-    "Playbook Video Preview": "Playbook Video Preview",
     "Playbook LumaAIClient": "Playbook LumaAI Client",
     "Playbook Text2Video": "Playbook Luma Video",
     "Playbook Image2Video": "Playbook Image to Video",
